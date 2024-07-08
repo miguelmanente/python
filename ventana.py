f124de7746ceda5b5e64e1f21ec9c657ef057302
Miguel Manente <manente2011@gmail.com>
2024-07-08 20:11:12 -0300

    crear ventanas

---
+++ ventana.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+def crearVentana():
+    #Crear una nueva ventana 
+    ventanaTop = tk.Toplevel(ventana)
+    ventanaTop.title = "Ventana top_level"
+    ventanaTop.geometry("400x200")
+
+#Crear ventana Principal
+ventana =tk.Tk()
+ventana.title("Ventana Principal")
+ventana.configure(bg="#C6D9E3")
+ventana.geometry("600x400")
+
+#Crear un boton que abra la ventana secundaria
+boton = tk.Button(ventana, text="Abrir Ventana", command=crearVentana)
+boton.pack()
+
+#Iniciar el buble principal de la aplicación
+ventana.mainloop()

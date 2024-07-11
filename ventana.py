0c603c5ca08d0aa215e6ea17d16b36da0b9b75e0
Miguel Manente <manente2011@gmail.com>
2024-07-11 16:03:04 -0300

    Ubica elementos

--- ventana.py
+++ ventana.py
@@ -15,6 +15,7 @@ ventana.geometry("600x400")
 #Crear un boton que abra la ventana secundaria
 boton = tk.Button(ventana, text="Abrir Ventana", command=crearVentana)
 boton.pack()
+boton.place(x=50, y=200)  # Se usa para ubicar el elemento boton en la ventana
 
 #Iniciar el buble principal de la aplicación
 ventana.mainloop()

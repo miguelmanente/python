a125c20acf5204e064bd3e8c82f5c314ea082710
Miguel Manente <manente2011@gmail.com>
2024-10-28 15:26:37 -0300

    Sistemas de ventas

---
+++ SistemasVentas/index.py
@@ -0,0 +1,6 @@
+from manager import Mananger
+
+if __name__=="__main__":
+    app =Mananger()
+    app.mainloop()
+    
---
+++ SistemasVentas/manager.py
@@ -0,0 +1,39 @@
+
+from tkinter import Tk, Frame
+from container import Container
+
+class Mananger(Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Caja registradora version 1.0")
+        self.resizable(False, False)
+        self.configure(bg="#C6D9E3")
+        self.geometry("800x400+120+20")
+
+        self.container = Frame(self, bg="#C6D9E3" )
+        self.container.pack(fill="both", expand=True)
+
+        self.frames = {
+            Container: None
+        }
+
+        self.load_frames()
+
+        self.show_frame(Container)
+
+
+    def load_frames(self):
+        for FrameClass in self.frames.keys():
+            frame = FrameClass(self.container, self)
+            self.frames[FrameClass] = frame
+    
+    def show_frame(self, frame_class):
+        frame =self.frames[frame_class]
+        frame.tkraise()
+    
+def main():
+    app = Mananger()
+    app.mainloop()
+
+if __name__ =="__main__":
+    main()
